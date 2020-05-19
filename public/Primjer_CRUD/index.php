90a967c0ae675d15d5cccd77b066da9b364d1966
mrvic <mrvic.predrag@gmail.com>
2020-05-19 18:58:25 +0200

    AlgebraBox init

--- public/Primjer_CRUD/index.php
+++ public/Primjer_CRUD/index.php
@@ -8,6 +8,7 @@ include 'functions.php';
 <?=template_header('Home')?>
 
 <div class="content">
+    <h5>Preuzeto sa: <a href="https://codeshack.io/crud-application-php-pdo-mysql/">https://codeshack.io/crud-application-php-pdo-mysql/</a></h5>
 	<h2>Studenti</h2>
 	<p>Ispis svih studenata</p>
 </div>
